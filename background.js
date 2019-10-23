f5a3e68aef7dea127787582661d8215f5578b817
Imandra <indigoimandra@gmail.com>
2019-10-23 12:24:27 -0700

    Correct background.js file

--- background.js
+++ background.js
@@ -1,10 +1,11 @@
-{
-  "name": "Squarespace Product Type Highlighter",
-  "version": "1.0",
-  "description": "Click to highlight products on a Squarespace Products Page and view their type.",
-  "background": {
-    "scripts": ["background.js"],
-    "persistent": false
-  },
-  "manifest_version": 2
-  }
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+  });
+});
