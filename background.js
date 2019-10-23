ad007b0f9cdabc4fb3529dd019a16406c9f03940
Imandra <indigoimandra@gmail.com>
2019-10-23 12:22:55 -0700

    Update background.js spacing

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 {
-    "name": "Squarespace Product Type Highlighter",
-    "version": "1.0",
-    "description": "Click to highlight products on a Squarespace Products Page and view their type.",
-    "background": {
-      "scripts": ["background.js"],
-      "persistent": false
-    },
-    "manifest_version": 2
+  "name": "Squarespace Product Type Highlighter",
+  "version": "1.0",
+  "description": "Click to highlight products on a Squarespace Products Page and view their type.",
+  "background": {
+    "scripts": ["background.js"],
+    "persistent": false
+  },
+  "manifest_version": 2
   }
