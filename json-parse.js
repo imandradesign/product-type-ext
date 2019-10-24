bcc6af45e392536a3cecd34f6605f048cb2a2cae
Imandra <indigoimandra@gmail.com>
2019-10-24 15:52:43 -0700

    Change JSON fetch to an async function

--- json-parse.js
+++ json-parse.js
@@ -20,32 +20,38 @@ document.body.appendChild(div);
 /**********************************************************
 Finds the JSON URL and finds the product type for each item on the Products Page
 *********************************************************/
-
 // Finds page URL with slug and creates JSON URL
-let find_url = window.location.href;
-let json_url = find_url + '?format=json';
+var json_url = window.location.href + '?format=json';
 
-// Fetch the JSON from the URL and print logs to console
-const response = fetch(json_url);
-const myJson = response.json();
 
-// Save product['items'] into the variable "products" and print it to console.
-var products = myJson['items'];
+async function info() {
+    try {
+      // Fetch the JSON from the URL
+      const response = fetch(json_url);
+      const myJson = await response.json();
+    } catch (error) {
+        // Error handling
+        alert("This isn't working!");
+    } finally {
+        // After try/catch is done, do this whether there was error or not. Save product['items'] into the variable "products" and print it to console.
+        var products = myJson['items'];
 
-// Initialize the count for each product type.
-var physicalCount = 0;
-var digitalCount = 0;
-var serviceCount = 0;
-var giftCardCount = 0;
+        // Initialize the count for each product type.
+        var physicalCount = 0;
+        var digitalCount = 0;
+        var serviceCount = 0;
+        var giftCardCount = 0;
 
-for (var i = 0; products[i]; i++){
-  if (products[i].productType == 1){
-    physicalCount ++;
-    } else if (products[i].productType == 2){
-      digitalCount ++;
-    } else if (products[i].productType == 3){
-      serviceCount ++;
-    } else if (products[i].productType == 4){
-      giftCardCount ++;
+        for (var i = 0; products[i]; i++){
+          if (products[i].productType == 1){
+            physicalCount ++;
+            } else if (products[i].productType == 2){
+              digitalCount ++;
+            } else if (products[i].productType == 3){
+              serviceCount ++;
+            } else if (products[i].productType == 4){
+              giftCardCount ++;
+            }
+        }
     }
 }
