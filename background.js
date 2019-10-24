31536900509554a1f77005e6dbd190248bd4ead3
Imandra <indigoimandra@gmail.com>
2019-10-24 13:22:09 -0700

    Add DIV to json-parse file and separate styles into styles.css

--- background.js
+++ background.js
@@ -1,6 +1,19 @@
 
-document.addEventListener('DOMContentLoaded', function(){
-  chrome.tabs.executeScript({
-				file: 'json-parse.js'
-			});
+chrome.browserAction.onClicked.addListener(function(tab){
+  function enable(tab){
+    chrome.tabs.executeScript({
+    	file: 'json-parse.js'
+    });
+    chrome.tabs.insertCSS({
+      file: 'blocks.css'
+    });
+  };
+
+  function disable(tab){
+    var determineOverlay = document.getElementById('overlay');
+
+    if (determineOverlay !== null){
+      document.getElementById('overlay').remove();
+    }
+  };
 });
--- json-parse.js
+++ json-parse.js
@@ -1,5 +1,3 @@
-let debug = 1;
-
 // Finds page URL with slug and creates JSON URL
 let find_url = window.location.href;
 let json_url = find_url + '?format=json';
@@ -8,25 +6,8 @@ let json_url = find_url + '?format=json';
 const response = fetch(json_url);
 const myJson = response.json();
 
-// If debug == 1:
-// Print to console, the JSON that was pulled from the URL
-if(debug == 1){
-    console.log("URL used:" + json_url);
-    console.log("JSON from the URL:");
-    console.log(myJson);
-}
-
 // Save product['items'] into the variable "products" and print it to console.
 var products = myJson['items'];
-
-// If debug == 1:
-// Print all of the products pulled in JSON format
-if(debug == 1){
-    console.log('\n' + "Products in JSON format:");
-    console.log(products);
-    console.log('\n');
-}
-
 
 // Initialize the count for each product type.
 var physicalCount = 0;
@@ -34,44 +15,27 @@ var digitalCount = 0;
 var serviceCount = 0;
 var giftCardCount = 0;
 
-for(var i=0; products[i]; i++){
-
-    // If debug == 1, print the current product number in iteration.
-    if(debug == 1){
-        console.log("Tallying Product Number:" + i);
-    }
-
-    if (products[i].productType == 1){
-        // If product[].type == 1 -> Physical
-        if(debug == 1){console.log("--> Physical Product" + '\n')}
-        physicalCount ++;
-    }
-    else if (products[i].productType == 2){
-        // If product[].type == 2 -> Digital
-        if(debug == 1){console.log("--> Digital Product" + '\n')}
-        digitalCount ++;
-    }
-    else if (products[i].productType == 3){
-        // If product[].type == 3 -> Service
-        if(debug == 1){console.log("--> Service Product" + '\n')}
-        serviceCount ++;
-    }
-    else if (products[i].productType == 4){
-        // If product[].type == 4 -> Gift Card
-        if(debug == 1){console.log("--> Gift Card Product" + '\n')}
-        giftCardCount ++;
+for (var i = 0; products[i]; i++){
+  if (products[i].productType == 1){
+    physicalCount ++;
+    } else if (products[i].productType == 2){
+      digitalCount ++;
+    } else if (products[i].productType == 3){
+      serviceCount ++;
+    } else if (products[i].productType == 4){
+      giftCardCount ++;
     }
 }
 
-// Print the tallied up total of each product type to console
-console.log("Physical Products: " + physicalCount);
-console.log("Digital Products: " + digitalCount);
-console.log("Service Products: " + serviceCount);
-console.log("Gift Card Products: " + giftCardCount);
+// Adding DIV to page
+var div = document.createElement('div');
 
-
-
-// Filter JSON data to provide only the "ProductType" inforation
-
-
-// Parse the number of each "ProductType" values to find a total for each
+// Generate HTML within div
+div.innerHTML = "<div id='overlay' onclick='off()'>\
+  <div id='text'>\
+  <p>Physical Products:</p><p id='physicalCount'></p>\
+  <p>Digital Products:</p><p id='digitalCount'></p>\
+  <p>Service Products:</p><p id='serviceCount'></p>\
+  <p>Gift Cards:</p></div><p id='giftCardCount'></p>\
+  <div id='button' onclick='off()'>CLOSE</button></div>\
+</div>";
