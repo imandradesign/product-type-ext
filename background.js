389bb9f8324325b244436fae6636d0c7b05abbe1
Imandra <indigoimandra@gmail.com>
2019-10-29 14:35:45 -0700

    Update background.js for keeping track of multiple tabs

--- background.js
+++ background.js
@@ -1,17 +1,13 @@
-// Variable to determine if extension is active or inactive
-var isActive = false;
+// Variables for keeping track of browser tabs
 var browserTabs = [];
 
 // Enable browser extension
 function enable(tab){
-  isActive = true;
-
-  chrome.browserAction.setIcon({path: "img/box48.png"});
-
+  browserTabs[tab].state = true;
+  chrome.browserAction.setIcon({path: "img/box48.png", tabId:browserTabs[tab].id});
   chrome.tabs.executeScript({
     file: 'json-parse.js'
   });
-
   chrome.tabs.insertCSS({
     file: 'styles.css'
   });
@@ -19,27 +15,55 @@ function enable(tab){
 
 // Disable browser extension
 function disable(tab){
-  isActive = false;
-
-  chrome.browserAction.setIcon({path: "img/box48-b.png"});
-
+  browserTabs[tab].state = false;
+  chrome.browserAction.setIcon({path: "img/box48-b.png", tabId:browserTabs[tab].id});
   chrome.tabs.executeScript({
     file: 'remove.js'
   });
 };
 
-// Clicking the extension icon runs the json-parse.js or remove.js file to enable or disable
+// Click function on extension that checks browser IDs
 chrome.browserAction.onClicked.addListener(function(tab){
-  if (isActive === false){
-    enable();
-  } else if (isActive === true){
-    disable();
-  }
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    var currentTab = tabs[0].id;
+    var alreadyAdded = false;
+
+    for (let i = 0; i < browserTabs.length; i++){
+      if (browserTabs[i].id === currentTab){
+        alreadyAdded = true;
+      }
+    }
+
+    if (alreadyAdded === false){
+      browserTabs.push({id:currentTab , state:true});
+
+      for (let i = 0; i < browserTabs.length; i++){
+        if (browserTabs[i].id === currentTab){
+          enable(i);
+        }
+      }
+    } else if (alreadyAdded === true){
+      for (let i = 0; i < browserTabs.length; i++){
+        if (browserTabs[i].id === currentTab){
+          if (browserTabs[i].state === false){
+            enable(i);
+          } else if (browserTabs[i].state === true){
+            disable(i);
+          }
+        }
+      }
+    }
+  });
 });
 
 // Disable extension when navigating to a new page
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if(changeInfo.status === "loading") {
-    disable();
+    for (var i = 0; i < browserTabs.length; i++){
+      if (browserTabs[i].id === currentTab){
+        browserTabs[i].state = false;
+        chrome.browserAction.setIcon({path: "img/box48-b.png", tabId:browserTabs[tab].id});
+      }
+    }
   };
 });
