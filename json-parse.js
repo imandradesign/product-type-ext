6b8c70ed73247f80647e088ca2a10ab83c2c81ec
Imandra <indigoimandra@gmail.com>
2019-10-30 12:31:12 -0700

    Update font, add colored squares, and remove text outline

--- json-parse.js
+++ json-parse.js
@@ -2,13 +2,7 @@
 var link = document.createElement('link');
 link.setAttribute('rel', 'stylesheet');
 link.setAttribute('type', 'text/css');
-link.setAttribute('href', 'https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700');
-document.head.appendChild(link);
-
-var link = document.createElement('link');
-link.setAttribute('rel', 'stylesheet');
-link.setAttribute('type', 'text/css');
-link.setAttribute('href', 'https://fonts.googleapis.com/css?family=Roboto&display=swap');
+link.setAttribute('href', 'https://fonts.googleapis.com/css?family=Cabin|Montserrat&display=swap');
 document.head.appendChild(link);
 
 /********************************************************
